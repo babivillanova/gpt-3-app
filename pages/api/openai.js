43409964a2d6c58cf173fd81e21d38fa56f3bca7
Barbara Villanova <villanovaarch@gmail.com>
2022-11-30 21:28:56 -0300

    Initial commit from Create Next App

---
+++ pages/api/openai.js
@@ -0,0 +1,39 @@
+const OpenAI = require('openai-api');
+const openai = new OpenAI(process.env.OPENAI_API_KEY);
+
+export default async (req, res) => {
+  let prompt = `Artist: ${req.body.name}\n\nLyrics:\n`;
+  const gptResponse = await openai.complete({
+    engine: 'davinci',
+    prompt: prompt,
+    maxTokens: 50,
+    temperature: 0.7,
+    topP: 1,
+    presencePenalty: 0,
+    frequencyPenalty: 0.5,
+    bestOf: 1,
+    n: 1
+});
+  
+  res.status(200).json({text: `${gptResponse.data.choices[0].text}`})
+}
+
+
+
+//para perguntas de entrevistas
+// const { Configuration, OpenAIApi } = require("openai");
+
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+// const openai = new OpenAIApi(configuration);
+
+// const response = await openai.createCompletion({
+//   model: "text-davinci-003",
+//   prompt: "Create a list of 8 questions for my interview with a science fiction author:",
+//   temperature: 0.5,
+//   max_tokens: 150,
+//   top_p: 1.0,
+//   frequency_penalty: 0.0,
+//   presence_penalty: 0.0,
+// });
